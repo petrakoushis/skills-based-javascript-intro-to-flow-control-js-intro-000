18396ebd34d9f9c01c47c19ce87ebfb98075d400
peter vassiliades <peter.vassiliades@gmail.com>
2019-02-28 06:49:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,7 +21,7 @@ else if (age <= 12) {
 function ternaryTeenager(age) {
   return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager" ;
 }
-
+/*
 function switchAge(age) {
   switch (age) {
     case 13 : console.log("You are a teenager");
@@ -33,6 +33,27 @@ function switchAge(age) {
     case 17 : "You are a teenager"; break;
     case 18 : "You are a teenager"; break;
     case 19 : "You are a teenager";
+    break;
+    default : "You have an age";
+  }
+
+}
+*/
+function switchAge(age) {
+  switch (age) {
+    case 13 : console.log("You are a teenager");
+    break;
+    case 14 : console.log("You are a teenager");
+    break;
+    case 15 : console.log("You are a teenager"); 
+    break;
+    case 16 : console.log("You are a teenager"); 
+    break;
+    case 17 : console.log("You are a teenager"); 
+    break;
+    case 18 : console.log("You are a teenager"); 
+    break;
+    case 19 : console.log("You are a teenager");
     break;
     default : "You have an age";
   }
