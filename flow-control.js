8047d449cb2651bfbb8955b6d4761723c37f4aa5
peter vassiliades <peter.vassiliades@gmail.com>
2019-02-28 06:17:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,11 +17,11 @@ else if (age <= 12) {
   return "You are a kid";
 } else return "You are a grownup";
 }
-/*
+
 function ternaryTeenager(age) {
   return (age >= 13 && age <= 19) "You are a teenager" : "You are not a teenager" ;
 }
-
+/*
 function switchAge(age) {
 
 }
