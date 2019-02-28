750ed2f9959ee1b8bbc482e33dc82fb83d87bbac
peter vassiliades <peter.vassiliades@gmail.com>
2019-02-28 05:02:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,11 @@
-function basicTeenager(age) {
+function basicTeenager(age) {if (age < 13 && age < 19) {
+  return "You are a teenager!"; }
 
 }
+console.log(basicTeenager(15));
 
-function teenager(age) {
+function teenager(age) {if age < 13 && age < 19 {return console.log("You are a teenager!")}
+return console.log( "you are not a teenager")
 
 }
 
