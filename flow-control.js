07c307e969403c4b1f54a2f5d45e3dd7800e69c5
peter vassiliades <peter.vassiliades@gmail.com>
2019-02-28 06:52:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -55,7 +55,7 @@ function switchAge(age) {
     break;
     case 19 : console.log("You are a teenager");
     break;
-    default : console.log("You have an age");
+    default : "You have an age";
   }
 
 }
